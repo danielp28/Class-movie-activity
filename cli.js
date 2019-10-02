6f0bd3f705ffa58cf70e56d99a4b275614bc8049
Larkin <larkiningram@gmail.com>
2019-10-02 13:19:45 -0500

    prints user inputs

--- cli.js
+++ cli.js
@@ -1,1 +1,15 @@
 
+var search = process.argv[2];
+var term = process.argv.slice(3).join(" ");
+
+
+if ((!search) && (!term)) {
+    search = "actor";
+    term = "Ryan Gosling";
+}
+if (search === "actor") {
+    console.log("Actor:", term)
+}
+else if (search === "show") {
+    console.log("Show:", term)
+}
